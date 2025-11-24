ed6256c8baf7c6297558f8b25b2bafd9e95e181b
sharanabasavas546 <sharanabasavas546@gmail.com>
2025-11-24 16:22:03 +0530

    Create addition1.py

---
+++ addition1.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+    print("Sum is:", add(x, y))
